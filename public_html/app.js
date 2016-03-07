fcd3836b4e9b5d415e3fec23d6b2bde37cf57566
anton <anton.jyrkiainen@helsinki.fi>
2016-03-08 01:36:06 +0200

    turhaa poistettu

--- public_html/app.js
+++ public_html/app.js
@@ -42,8 +42,10 @@ function closestStop() {
     var count = 0, currentStop = "", currentStop = "", link = "", stack = [];
 
     for (var i = 0, max = text.length; i < max; i++) {
-        if (text.charAt(i) === ",") count++;
-        if (count === 3) currentStop = currentStop + text.charAt(i);
+        if (text.charAt(i) === ",")
+            count++;
+        if (count === 3)
+            currentStop = currentStop + text.charAt(i);
         if (count === 4) {
             var currentStopLat = parseFloat(text.substr(i + 1, 9)), currentStopLng = parseFloat(text.substr(i + 11, 9));
             var currentLatDifference = Math.abs(currentStopLat - lat), currentLngDifference = Math.abs(currentStopLng - lng);
@@ -54,18 +56,17 @@ function closestStop() {
             var newStop = new stop(currentStopLat, currentStopLng, currentStop, link, (currentLatDifference + currentLngDifference));
             stack.push(newStop), count++, i = i + 20, currentStop = "";
         }
-        if (count === 10) count = 0;
+        if (count === 10)
+            count = 0;
     }
-    console.log(skipAmount);
-    var compare = function (a, b) {
+    
+    stack.sort(function (a, b) {
         return b.difference - a.difference;
-    };
-    stack.sort(compare);
+    });
     for (var i = 0, max = skipAmount; i < max; i++) {
         stack.pop();
     }
     var s = stack.pop();
-    console.log(s.name);
     document.getElementById('info').innerHTML = "Lähin pysäkki: " + s.name;
     var a = document.getElementById('top');
     a.innerHTML = "Pysäkin aikataulut (ohjaa HSL:n sivuille)";
